f0fcf9a71251bb5da68f24a8c060ee13ef819194
Baratheeswar <103169760+Baratheeswar@users.noreply.github.com>
2022-04-23 21:42:29 +0530

    Register for validation added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormControl , FormControlName, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private formbuilder: FormBuilder, private router: Router) {
+
+    this.registerForm = this.formbuilder.group({
+      'name': ['',Validators.required],
+      'username': ['',Validators.required],
+      'email': ['',Validators.required],
+      'password': ['',Validators.required],
+      'confirmpassword': ['',Validators.required]
+    })
+
   }
 
+  ngOnInit(): void { }
+
+  registerUser(){
+    this.router.navigate(['../login'])
+  }
 }
